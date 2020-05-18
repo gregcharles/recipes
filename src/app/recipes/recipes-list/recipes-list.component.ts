8712f64b34f9b10c1b915886da96f49f389969b0
Greg Charles <gregcharles@javaranch.com>
2020-05-17 21:26:53 -0700

    Lecture 51: Recipe model

--- src/app/recipes/recipes-list/recipes-list.component.ts
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Recipe} from '../recipes.model';
 
 @Component({
   selector: 'app-recipes-list',
@@ -6,6 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipes-list.component.css']
 })
 export class RecipesListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+        'Veggie Burger',
+        'Heat up  a veggie burger',
+        'https://assets.bonappetit.com/photos/57acae2d1b33404414975121/16:9/w_5120,c_limit/ultimate-veggie-burger.jpg'),
+    new Recipe(
+          'Bleeding Armadillo Cake',
+          'Eww, gross!',
+          'https://images.notquitenigella.com/images/armadillo-cake/__red-velvet-armadillo-cake-33.jpg')
+  ];
 
   constructor() { }
 
