c1638a4f32e0642e7118183d004d0974df348807
Greg Charles <greg.charles@accenture.com>
2020-05-31 14:29:19 -0700

    Lecture 86: recipe item iteration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'recipes';
   selection = 'recipes';
 
   onHeaderSelection(event: string) {
